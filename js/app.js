b10106fe154cd77c3e54b8701fb19ee3929331ec
kinwl64 <kinwailam@192.168.1.4>
2014-08-20 01:32:37 -0400

    X

---
+++ js/app.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    playHadouken();    
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show()
+    .animate(
+      {'left': '300px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '-212px');
+      }
+    );
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  })
+
+	$(document).keydown(function(e){
+		if (e.keyCode == 88) {
+//      playEyeoftiger();
+        $('.ryu-ready').hide();
+     	$('.ryu-cool').show(); 
+	}	
+
+	}).keyup(function(e) {
+    	if (e.keyCode == 88) {
+//	    $('#eye-of-tiger')[0].pause();
+//	    $('#eye-of-tiger')[0].load();
+        $('.ryu-cool').hide();
+        $('.ryu-still').show();
+    }
+  });
+});
+
+    
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
+
+//function playEyeoftiger () {
+//  $('#eye-of-tiger')[0].volume = 0.5;
+//  $('#eye-of-tiger')[0].load();
+//  $('#eye-of-tiger')[0].play();
+//}
+
+
+
